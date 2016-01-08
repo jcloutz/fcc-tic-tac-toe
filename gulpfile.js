fdc4e1294212455697344cf49d744b3571ade668
Jeremy Cloutier <jcloutier24@yahoo.com>
2016-01-08 00:31:13 -0500

    moved to template based system and asset management through settings.json file located in root directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-var gulp = require('gulp');
-var del = require('del');
-var babel = require('gulp-babel');
-var eslint = require('gulp-eslint');
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var nodemon = require('gulp-nodemon');
-var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
-var reload = browserSync.reload;
+var gulp = require('gulp'),
+    del = require('del'),
+    babel = require('gulp-babel'),
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    prefix = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    nodemon = require('gulp-nodemon'),
+    watch = require('gulp-watch'),
+    plumber = require('gulp-plumber'),
+    replace = require('gulp-replace'),
+    reload = browserSync.reload,
+    rename = require('gulp-rename'),
+    fs = require('fs');
 
 var paths = {
     source: {
@@ -27,8 +30,28 @@ gulp.task('clean', () => {
 });
 
 gulp.task('html', () => {
-    gulp.src([paths.source.base + '*.html'])
+    var settings = JSON.parse(fs.readFileSync('./settings.json', {encoding: 'utf8'}));
+    var content = fs.readFileSync(paths.source.base + 'index.html', {encoding: 'utf8'});
+
+    var metatags = settings.html.metatags.join('\n');
+    var htmlClasses = settings.html.classes.join(' ');
+    var stylesheets = settings.css.map(function(stylesheet) {
+      return '<link rel="stylesheet" href="' + stylesheet + '">';
+    }).join('\n');
+    console.log(htmlClasses);
+    var scripts = settings.javascript.map(function(script) {
+      return '<script src="' + script + '"></script>';
+    }).join('\n');
+
+    gulp.src('template.html')
         .pipe(plumber())
+        .pipe(replace('%TITLE%', settings.html.title))
+        .pipe(replace('%CLASSES%', htmlClasses))
+        .pipe(replace('%METATAGS%', metatags))
+        .pipe(replace('%STYLESHEETS%', stylesheets))
+        .pipe(replace('%CONTENT%', content))
+        .pipe(replace('%SCRIPTS%', scripts))
+        .pipe(rename('index.html'))
         .pipe(gulp.dest(paths.output.base))
         .pipe(browserSync.stream());
 })
@@ -61,7 +84,7 @@ gulp.task('default', ['html', 'css', 'scripts'], function() {
             baseDir: paths.output.base
         }
     });
-    gulp.watch(paths.source.base + '*.html', ['html']);
+    gulp.watch([paths.source.base + 'index.html', 'template.html', 'settings.json'], ['html']);
     gulp.watch(paths.source.css + "*.scss", ['css']);
     gulp.watch(paths.source.js + '*.js', ['scripts']);
 
