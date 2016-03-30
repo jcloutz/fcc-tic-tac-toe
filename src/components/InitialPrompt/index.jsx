b190b6fd9615678e4a71eeec8c5d89918453b94f
Jeremy Cloutier <jcloutier24@yahoo.com>
2016-03-30 14:01:55 -0400

    Working forward type

--- src/components/InitialPrompt/index.jsx
+++ src/components/InitialPrompt/index.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import TypeWriter from '../TypeWriter'
-import { hashHistory } from 'react-router'
+// import { hashHistory } from 'react-router'
 
-const InitialPrompt = () => (
-  <div>
-    <TypeWriter
-      message='DO YOU WANT TO PLAY A GAME?'
-      startDelay={2000}
-      keystrokeDelay={80}
-      enterTimeout={500}
-      leaveTimeout={500}
-    />
-    <div className='prompt__button-container'>
-      <button onClick={() => {
-        hashHistory.push('/players')
-      }}>
-        Yes
-      </button>
-      <button>No</button>
-    </div>
-  </div>
-)
+class InitialPrompt extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      typewriterStatus: 'typing'
+    }
+    this.handleConfirmClick = this.handleConfirmClick.bind(this)
+  }
+
+  handleConfirmClick (e) {
+    this.setState({typewriterStatus: 'deleting'})
+  }
+
+  render () {
+    return (
+      <div>
+        <TypeWriter
+          message='DO YOU WANT TO PLAY A GAME?'
+          status={this.state.typewriterStatus}
+          startDelay={2000}
+          keystrokeDelay={80}
+          enterTimeout={500}
+          leaveTimeout={500}
+        />
+        <div className='prompt__button-container'>
+          <button onClick={this.handleConfirmClick}>
+            Yes
+          </button>
+          <button>No</button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default InitialPrompt
--- src/components/TypeWriter/index.jsx
+++ src/components/TypeWriter/index.jsx
@@ -7,49 +7,100 @@ export default class TypeWriter extends React.Component {
     super(props)
     this.carraige = null // will be used for interval
     this.state = {
+      status: 'idle',
       message: this.props.message,
       visibleChars: '',
       hiddenChars: this.props.message
     }
+    this.typeMessage = this.typeMessage.bind(this)
+    this.deleteMessage = this.deleteMessage.bind(this)
   }
 
-  componentDidMount () {
-    // Create promise to begin rendering typed characters after start delay.
+  typeMessage () {
+    // this.setState({typing: true})
     let typewriter = new Promise((resolve, reject) => {
       this.typewriter = setTimeout(() => {
-        resolve(true)
+        this.setState({status: 'typing'}, () => {
+          resolve(true)
+        })
       }, this.props.startDelay)
     })
     // start typing characters
     typewriter.then(() => {
       let message = this.state.message.split('')
       let counter = 0
-
+      let _this = this
       // Declare inteveral at class level to allow componentWillUnmount to cancel if necessary
       this.carraige = setInterval(() => {
-        if (message.length > 0) {
+        if (message.length > 0 && _this.state.status === 'typing') {
+          let complete = (counter === message.length)
           this.setState({
             visibleChars: this.state.visibleChars + message.shift(),
-            hiddenChars: message.join('')
+            hiddenChars: message.join(''),
+            status: complete ? 'idle' : 'typing'
           })
           counter++
         } else {
+          console.log('typing finished')
           clearInterval(this.carraige)
         }
       }, this.props.keystrokeDelay)
     })
   }
 
+  deleteMessage () {
+    let typewriter = new Promise((resolve, reject) => {
+      this.typewriter = setTimeout(() => {
+        this.setState({status: 'deleting'}, () => {
+          resolve(true)
+        })
+      }, this.props.startDelay)
+    })
+    // start typing characters
+    typewriter.then(() => {
+      let { visibleChars, hiddenChars } = this.state
+      let counter = 0
+      let _this = this
+
+      // Declare inteveral at class level to allow componentWillUnmount to cancel if necessary
+      this.carraige = setInterval(() =>
+        debugger
+        console.log('deleting')
+        if (visibleChars.length > 0 && _this.state.status === 'deleting') {
+          let complete = visibleChars.length === 0
+          this.setState({
+            visibleChars: visibleChars.join(''),
+            hiddenChars: visibleChars.pop() + hiddenChars,
+            status: complete ? 'idle' : 'typing'
+          })
+          counter++
+        } else {
+          console.log('typing finished')
+          clearInterval(this.carraige)
+        }
+      }, this.props.keystrokeDelay)
+    })
+  }
+
+  componentDidMount () {
+    // Create promise to begin rendering typed characters after start delay.
+    // this.setState({typing: true, status: 'idle'})
+  }
+
   componentWillUnmount () {
-    // TODO implement reverse type effect.
     // clear timeout and intervals
     clearTimeout(this.typewriter)
     clearInterval(this.carraige)
-    console.log('leaving')
-    return false
   }
 
   render () {
+    if (this.props.status === 'typing' && this.state.status !== 'typing') {
+      console.log('type message triggered')
+      this.typeMessage()
+    } else if (this.props.status === 'deleting' && this.state.status !== 'deleting') {
+      console.log('delete message triggered')
+      this.deleteMessage()
+    }
     let counter = 0
     const {visibleChars, hiddenChars} = this.state
     const visChars = visibleChars.split('').map((char) => (
@@ -80,6 +131,7 @@ export default class TypeWriter extends React.Component {
 const { number, string } = React.PropTypes
 
 TypeWriter.propTypes = {
+  status: string,
   message: string,
   startDelay: number,
   keystrokeDelay: number,
