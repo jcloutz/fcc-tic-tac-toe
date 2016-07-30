f78afcafefb3e1208050f1f5c76378b4a7bde785
Jeremy Cloutier <jcloutier24@yahoo.com>
2016-07-30 00:26:47 -0400

    removed typings from repo

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/react-global/index.d.ts" />
-/// <reference path="browser/ambient/react-redux/index.d.ts" />
-/// <reference path="browser/ambient/react-router/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/react-global/index.d.ts" />
-/// <reference path="main/ambient/react-redux/index.d.ts" />
-/// <reference path="main/ambient/react-router/index.d.ts" />
